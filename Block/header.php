a07173583e01253d92cdc2fec263dfb23b7fd8b7
danilsocol <danilsocol-2002@mail.ru>
2021-05-24 21:26:26 +0500

    Add block

---
+++ Block/header.php
@@ -0,0 +1,14 @@
+ 
+        <header>
+                 <div id= logo>
+                    <a href="/Main.php" title="Перейти на главную">  Каледарь-Памятка </a>
+                 </div>
+
+                 <div id=menuHead>
+                     Что то там
+                 </div>
+
+                 <div id=regAndAuth>
+                    <a href="reg.php">Регистрация</a>  |  <a href="aut.php">Авторизация</a>
+                </div>
+        </header>
